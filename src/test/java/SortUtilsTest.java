5ebdfa348184fb93bebe827c67d2b673d6f3633b
dukesodope <ductrantrung10@gmail.com>
2025-01-10 21:05:50 +0700

    2nd

--- src/test/java/SortUtilsTest.java
+++ src/test/java/SortUtilsTest.java
@@ -2,7 +2,7 @@ import org.example.SortUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertArrayEquals;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SortUtilsTest {
